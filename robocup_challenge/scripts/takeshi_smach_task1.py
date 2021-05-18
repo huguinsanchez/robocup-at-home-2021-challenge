8594606c7f432139266dedd6409158078efded8d
Mitzi anaid Ramírez Estrada <mitzi.ramest@gmail.com>
2021-05-18 15:36:06 -0500

    preliminary tests

--- robocup_challenge/scripts/takeshi_smach_task1.py
+++ robocup_challenge/scripts/takeshi_smach_task1.py
@@ -878,4 +878,5 @@ if __name__== '__main__':
         smach.StateMachine.add('POST_TABLE',    Post_table(),   transitions = {'failed':'PRE_TABLE2',  'succ': 'GO_BOX',       'tries':'INITIAL'}) 
         smach.StateMachine.add('PRE_TABLE2',    Pre_table2(),    transitions = {'failed':'PRE_TABLE2',    'succ': 'GRASP_TABLE',  'tries':'INITIAL'}) 
 
+
     outcome = sm.execute()   
